7f8b219631f20ccd8d10dcaad8e0c0b790eeb576
Jomon Johnson <jomon.entero@gmail.com>
2019-03-06 14:39:57 +0530

    init

---
+++ src/IndiaStates.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 6/3/19
+ * Time: 2:37 PM
+ */
+
+namespace codexten\php\india\states;
+
+
+class IndiaStates
+{
+    public static function dropdown()
+    {
+        return require(__DIR__ . '/india-states.php');
+    }
+
+}
